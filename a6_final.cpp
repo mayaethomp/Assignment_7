a783309b6109bcae38d38ca94dac7ea956190be7
Maya Thompson <mayaethomp@gmail.com>
2024-08-02 01:15:58 -0700

    Initial commit

---
+++ a6_final.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+
+// Declare and initialize variables
+
+
+// Arrays
+
+
+// Structures
+struct Drinks {
+    string name;
+    float cost;
+    int inventory;
+};
+
+
+// Function prototypes
+
+
+int main() {
+
+    
+/* ------------ DRINKS DATA --------------*/
+
+    Drinks cola = {"Cola", 0.65, 20};
+    Drinks rootBeer = {"Root Beer", 0.70, 20};
+    Drinks grapeSoda = {"Grape Soda", 0.75, 20};
+    Drinks lemonLime = {"Lemon Lime", 0.85, 20};
+    Drinks water = {"Water", 0.90, 20};
+    
+
+
+
+    return 0;
+}
